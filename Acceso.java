6e71847d0d82065893287ce2292be37cde07101c
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2018-03-05 19:21:45 +0100

    Código realizado hasta el momento en el proyecto documentado.

--- Acceso.java
+++ Acceso.java
@@ -1,3 +1,8 @@
+/**
+ * Esta clase representa un acceso al servidor. Se podrá consultar la ip desde donde se consulta, la fecha y hora, la URL consultada y el código HTTP del sitio web.
+ * author (Jorge Jaular Lasaga)
+ * version (05/03/2018)
+ */
 public class Acceso
 {
     private String ip;
@@ -8,54 +13,93 @@ public class Acceso
     private int minutos;  
     private String url;
     private int codigoHTTP;
+    
+    /**
+     * Constructor de la clase Acceso. Lee los datos del acceso al servidor.
+     * @param acceso De tipo String, son los datos de acceso.
+     */
     public Acceso(String acceso)
     {
         String[] elementosAcceso = acceso.split(" ");
         ip = elementosAcceso[0];
+        // Este substring quita el primer corchete de apertura.
         ano = Integer.parseInt(elementosAcceso[1].substring(1, elementosAcceso[1].length()));
         mes = Integer.parseInt(elementosAcceso[2]);
         dia = Integer.parseInt(elementosAcceso[3]);
         hora = Integer.parseInt(elementosAcceso[4]);
+        // Este substring quita el último corchete de cierre.
         minutos = Integer.parseInt(elementosAcceso[5].substring(0, elementosAcceso[5].length() - 1));
         url = elementosAcceso[6];
         codigoHTTP = Integer.parseInt(elementosAcceso[7]);
     }
     
+    /**
+     * Muestra la IP del lugar visitado.
+     * @return ip Devuelve la IP.
+     */
     public String getIp()
     {
         return ip;
     }
     
+    /**
+     * Muestra el año de acceso.
+     * @return ano Devuelve el año de acceso en formato numérico.
+     */    
     public int getAno() 
     {
         return ano;
     }
     
+    /**
+     * Muestra el mes de acceso.
+     * @return mes Devuelve el mes de acceso en formato numérico.
+     */
     public int getMes()
     {
         return mes;
     }
     
+    /**
+     * Muestra el día de acceso.
+     * @return dia Devuelve el día de acceso en formato numérico.
+     */
     public int getDia()
     {
         return dia;
     }
     
+    /**
+     * Muestra la hora de acceso al servidor.
+     * @return hora Devuelve la hora de acceso.
+     */
     public int getHora()
     {
         return hora;
     }
     
+    /**
+     * Muestra el minuto de acceso al servidor.
+     * @return minutos Devuelve el minuto de acceso.
+     */
     public int getMinutos()
     {
         return minutos;
     }
     
+    /**
+     * Muestra la URL visitada.
+     * @return url Devuelve una cadena del lugar visitado.
+     */    
     public String getUrl()
     {
         return url;
     }
     
+    /**
+     * Muestra el código HTTP del lugar visitado por la persona.
+     * @return codigoHTTP Devuelve el código HTTP del lugar visitado en formato numérico.
+     */
     public int getCodigoHTTP()
     {
         return codigoHTTP;
--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -2,15 +2,31 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * Esta clase analiza los accesos al servidor. Se compondrá de dos métodos, uno
+ * que importará un documento de texto con los accesos al servidor y otro que 
+ * obtendrá qué hora tiene más accesos.
+ * 
+ * @author (Jorge Jaular Lasaga) 
+ * @version (05/03/2018)
+ */
 public class AnalizadorAccesosAServidor
 {
     private ArrayList<Acceso> accesos;  
     
+    /**
+     * El constructor de la clase. Crea un analizador de accesos al servidor.
+     */
     public AnalizadorAccesosAServidor() 
     {
         accesos = new ArrayList<>();
     }
-        
+    
+    /**
+     * Este método permite cargar los datos de acceso al servidor.
+     * 
+     * @param nombreArchivo Se introduce el nombre del archivo que servirá para cargar los datos.
+     */
     public void analizarArchivoDeLog(String archivo)
     {
         accesos.clear();
@@ -28,7 +44,12 @@ public class AnalizadorAccesosAServidor
             System.out.println("Ocurrio algun error al leer el archivo.");
         }
     }
-        
+    
+    /**
+     * Este método comprueba a qué hora se produjeron más conexiones o accesos al servidor. Devuelve la hora con más conexiones, y en caso de empate devuelve la hora más alta. Si no hay conexiones
+     * devuelve -1.
+     * @return horaConMasAccesos Devuelve la hora con más conexiones, o -1 en caso de no haber importado datos. 
+     */
     public int obtenerHoraMasAccesos() 
     {
         int valorADevolver = -1;        
@@ -55,15 +76,21 @@ public class AnalizadorAccesosAServidor
         return valorADevolver;
     }   
     
+    /**
+     * Muestra la web más solicitada por la gente.
+     * @return webMasSolicitada Devuelve como cadena la web más solicitada.
+     */
     public String paginaWebMasSolicitada() 
     {
         return "";
     }
     
+    /**
+     * Muestra la IP del cliente con más accesos exitosos.
+     * @return clienteConMasAccesos Devuelve el cliente con más accesos.
+     */
     public String clienteConMasAccesosExitosos()
     {
         return "";
     }
-
-
 }
